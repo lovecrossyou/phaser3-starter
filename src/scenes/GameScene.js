96eb7b8fbece222a417e2a16c4ad062a07f46864
lovecrossyou <lovecrossyoutt@hotmail.com>
2020-02-24 22:10:13 +0800

    xx

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,15 +1,15 @@
-import Player from '../sprites/Player';
 class GameScene extends Phaser.Scene {
 
     constructor(test) {
         super({
             key: 'GameScene'
         });
+        this.platforms = null;
+        this.player = null;
     }
 
     preload() {
         // this.load.scenePlugin('animatedTiles', AnimatedTiles, 'animatedTiles', 'animatedTiles');
-
     }
 
     create() {
@@ -24,23 +24,46 @@ class GameScene extends Phaser.Scene {
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
 
-        this.player = new Player({
-            scene: this,
-            key: 'dude',
-            x: 16 * 6,
-            y: this.sys.game.config.height - 48 - 48
-        })
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+
     }
 
     update() {
         if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
             this.player.anims.play('left', true);
         }
         else if (this.cursors.right.isDown) {
-            this.player.anims.play('right', true);
+            this.player.setVelocityX(160);
+            this.player.play('right', true);
         }
-        else{
-            // this.player.anims.play('turn', true);
+        else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('idle', true);
         }
     }
 }
